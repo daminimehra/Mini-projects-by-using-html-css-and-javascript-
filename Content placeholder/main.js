eec0a88d002bd43b7ddd1583ca30bbe983233ba0
Damini Mehra <71871235+daminimehra@users.noreply.github.com>
2022-01-21 17:56:39 +0530

    Add files via upload

---
+++ Content placeholder/main.js
@@ -0,0 +1,25 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const namea = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img scr="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
+
+  title.innerHTML = `djvhvbdch ncn cj`;
+  excerpt.innerHTML = `hello chdifhsi fifiafh fhwefiffgdfb jhfdiaf. dsnfkdbsdkjvbkjckj jcbkjjx hbefdgfei . hfiehf`;
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
+  namea.innerHTML = `hasj dksa`;
+  date.innerHTML = ` nov 17, 2022`;
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
